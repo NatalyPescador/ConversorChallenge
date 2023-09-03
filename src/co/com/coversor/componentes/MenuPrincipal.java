671e1738973af7657fe3f1cdbf8ac41cffcd93df
Nataly Pescador <nataly.pescador@gmail.com>
2023-09-03 14:24:55 -0500

    Arreglando el diseño del menu principal

--- src/co/com/coversor/componentes/MenuPrincipal.java
+++ src/co/com/coversor/componentes/MenuPrincipal.java
@@ -4,32 +4,45 @@ import javax.swing.*;
 
 public class MenuPrincipal extends JFrame {
     private JLabel label1;
-    private JComboBox comboBox1;
-    private JButton boton1;
-    private JButton boton2;
+    private JComboBox menu;
+    private JButton botonAceptar;
+    private JButton botonCancelar;
 
     public MenuPrincipal() {
         //Ajustes generales de la interfaz
         setLayout(null);
+        setSize(300,160);
+        setResizable(false);
         setLocationRelativeTo(null);
         setTitle("Conversor Challenge");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //Ajustes JLabel
         label1 = new JLabel("Seleccione una opción de conversión ");
-        label1.setBounds(60, 30, 150, 20);
+        label1.setBounds(15, 12, 250, 20);
         add(label1);
 
         //Ajustes JComboBox
-        comboBox1 = new JComboBox();
-        comboBox1.setBounds(60, 60, 150, 20);
-        add(comboBox1);
+        menu = new JComboBox();
+        menu.setBounds(15, 38, 260, 25);
+        add(menu);
 
         //Agregando items al JComboBox
-        comboBox1.addItem("Conversor de moneda");
-        comboBox1.addItem("Conversor de temperatura");
+        menu.addItem("Conversor de moneda");
+        menu.addItem("Conversor de temperatura");
 
-        //
+        //Ajustes JButton
+        botonAceptar = new JButton("Aceptar");
+        botonAceptar.setBounds(50,75,85,25);
+        add(botonAceptar);
 
+        botonCancelar = new JButton("Cancelar");
+        botonCancelar.setBounds(150,75,85,25);
+        add(botonCancelar);
+    }
+
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.setVisible(true);
     }
 }
